1582f4ce31362fa79ff1c07673fde5e10187e1a9
vsap2 <vsap29@outlook.com>
2018-01-18 16:45:21 +0200

    unknown error!!?? implicits - done; UC & notifications - todo;

--- src/main/scala/EchoBot.scala
+++ src/main/scala/EchoBot.scala
@@ -1,20 +1,35 @@
+import java.time.LocalDateTime
+
 import UserActor.UserMessage
-//import akka.actor.Status.Success
+import info.mukel.telegrambot4s.api.ChatActions
 import akka.actor.{Actor, Props}
-import info.mukel.telegrambot4s.api.declarative.Commands
+import info.mukel.telegrambot4s.api.declarative.{Callbacks, Commands, Help, InlineQueries}
 import info.mukel.telegrambot4s.api.{Extractors, Polling, TelegramBot}
-//import info.mukel.telegrambot4s.methods.SendMessage
 import info.mukel.telegrambot4s.models.Message
-import model.Interface._
+import slick.jdbc.PostgresProfile.api._
 
-
-import scala.concurrent.Future
 import scala.io.Source
-import scala.util.Random
-import model._
-import repository.{Task,User}
+import repository._
+import java.time.Instant
 
-class TodoBotActor extends TelegramBot  with Polling with Commands with Actor {
+import info.mukel.telegrambot4s.Implicits._
+import info.mukel.telegrambot4s.api._
+import info.mukel.telegrambot4s.api.declarative.{Callbacks, InlineQueries}
+import info.mukel.telegrambot4s.methods._
+import info.mukel.telegrambot4s.models.UpdateType.Filters._
+import info.mukel.telegrambot4s.models._
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+
+
+
+class TodoBotActor extends TelegramBot
+                    with Polling
+                    with Commands
+                    with InlineQueries
+                    with Actor {
 
   lazy val token =
     scala.util.Properties
@@ -22,20 +37,11 @@ class TodoBotActor extends TelegramBot  with Polling with Commands with Actor {
     .getOrElse(Source.fromFile("bot.token").getLines().mkString)
 
 
+  val db = Database.forConfig("connect")
+  val userRepository = new UserRepository(db)
+  val taskRepository = new TaskRepository(db)
 
-//  onCommand("hello") { implicit msg =>
-//    reply("My token is SAFE!")
-//  }
-//
-//  val rng = new Random(System.currentTimeMillis())
-//  onCommand("random", "rand") { implicit msg =>
-//    withArgs {
-//      case Seq(Extractors.Int(n)) if n > 0 =>
-//        reply(rng.nextInt(n).toString)
-//      case _ =>
-//        reply("Invalid argumentヽ(ಠ_ಠ)ノ")
-//    }
-//  }
+
 
   onCommand("/hallo", "/bonjour", "/ciao", "/hola") {
     implicit msg =>
@@ -45,20 +51,24 @@ class TodoBotActor extends TelegramBot  with Polling with Commands with Actor {
       }
   }
 
+
   onCommand("/start"){ implicit msg =>
+    println("inside")
+    Await.result(db.run(UserTable.table.schema.create),Duration.Inf)
+    Await.result(db.run(TaskTable.table.schema.create),Duration.Inf)
+    println(" schema created")
     using(_.from){
      user =>
-       val u = User(user.username.get, user.id.toString)
+       println(user.username.get + "extracted")
+       val u = repository.User(user.username.get, user.id.toString)
        userRepository.contains(u).foreach{ contains =>
          if(contains == 0){
            userRepository insert u
+           println("Inserted")
            reply("Welcome " + user.firstName)
          }else reply("Nice to meet u again!" + user.firstName)
        }
     }
-  }
-  onCommand("help"){ implicit msg =>
-    InputParser.usrCtrlCmdsDescription.map(reply(_))
   }
 //  onCommand("info"){ implicit msg =>
 //  }
@@ -147,6 +157,92 @@ class TodoBotActor extends TelegramBot  with Polling with Commands with Actor {
 //    }
 //  }
 
+
+//  val timeouts = Seq(3, 5, 10, 30)
+//
+//  override def allowedUpdates = InlineUpdates ++ CallbackUpdates
+//
+//  def buildResult(timeout: Int, msg: String): InlineQueryResult = {
+//    InlineQueryResultArticle(s"$timeout", s"$timeout seconds",
+//      inputMessageContent = InputTextMessageContent(msg),
+//      description = s"Message will be deleted in $timeout seconds",
+//      replyMarkup = InlineKeyboardMarkup.singleButton(button(now)))
+//  }
+//
+//  def now = Instant.now().getEpochSecond
+//
+//  def button(t: Long) = InlineKeyboardButton.callbackData("⏳ left?", t + "")
+//
+//  onCallbackQuery {
+//    implicit cbq =>
+//      val left = cbq.data.map(_.toLong - now).getOrElse(-1L)
+//      ackCallback(s"$left seconds remaining.", cacheTime = 0)
+//  }
+
+//  onChosenInlineResult { implicit result =>
+//    val delay = result.resultId.toInt
+//    request(EditMessageReplyMarkup(
+//      inlineMessageId = result.inlineMessageId,
+//      replyMarkup = InlineKeyboardMarkup.singleButton(button(now + delay))))
+//
+//    system.scheduler.scheduleOnce(delay.seconds) {
+//      request(EditMessageText(
+//        text = "⌛ Expired",
+//        inlineMessageId = result.inlineMessageId))
+//    }
+//  }
+
+//  onInlineQuery { implicit q =>
+//    val results = if (q.query.isEmpty)
+//      Seq.empty
+//    else
+//      timeouts.map(buildResult(_, q.query))
+//    answerInlineQuery(results, 5)
+//  }
+
+  onInlineQuery { implicit q =>
+    val u = repository.User(q.from.username.get, q.from.id.toString)
+    userRepository.contains(u).foreach{ contains =>
+      if(contains == 0){
+        userRepository insert u
+        answerInlineQuery(
+          Seq(
+            InlineQueryResultArticle(
+              q.id,
+              "Third variant!",
+              InputTextMessageContent("Hello from the other side!!"),
+              InlineKeyboardMarkup(
+                Seq(
+                  Seq(
+                    InlineKeyboardButton.url("Shake it of baby", "https://www.youtube.com/watch?v=nfWlot6h_JM")))))))
+      }else {
+        answerInlineQuery(
+          Seq(
+            InlineQueryResultArticle(
+              q.id,
+              "Rolling in the deep!",
+              InputTextMessageContent("Try it!!"),
+              InlineKeyboardMarkup(
+                Seq(
+                  Seq(
+                    InlineKeyboardButton.url("Rolling in the deep, baby!", "https://www.youtube.com/watch?v=rYEDA3JcQqw")
+                  )
+                )
+              )
+            ),
+        InlineQueryResultArticle(
+              q.id,
+              "Shake it off",
+              InputTextMessageContent("Try it!!"),
+              InlineKeyboardMarkup(
+                Seq(
+                  Seq(
+                    InlineKeyboardButton.url("Shake it off, baby!", "https://www.youtube.com/watch?v=nfWlot6h_JM")))))))
+      }
+    }
+  }
+
+
   override def preStart(): Unit = {
     run()
   }
@@ -160,6 +256,6 @@ class TodoBotActor extends TelegramBot  with Polling with Commands with Actor {
 
 case class SendMessage(userMessage: UserMessage)(implicit val message: Message)
 
-object EchoBotActor{
+object TodoBotActor{
   def props(): Props = Props(new TodoBotActor())
 }
--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,15 +7,11 @@ import scala.concurrent.duration.Duration
 
 object Main {
 
-  val db = Database.forConfig("connect")
-  val userRepository = new UserRepository(db)
-  val taskRepository = new TaskRepository(db)
 
   def main(args: Array[String]): Unit = {
     println("Hello")
-    Await.result(db.run(UserTable.table.schema.create),Duration.Inf)
-    Await.result(db.run(TaskTable.table.schema.create),Duration.Inf)
+
     val actorSystem = ActorSystem("my-actor-system")
-    actorSystem.actorOf(EchoBotActor.props(), "echo-bot-actor")
+    actorSystem.actorOf(TodoBotActor.props(), "todo-bot-actor")
   }
 }
